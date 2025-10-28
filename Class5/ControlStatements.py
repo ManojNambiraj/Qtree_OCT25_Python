3dfcc4fa1264547f147cfbac8ce2e37de58af3d9
ManojNambiraj <postboxmano@gmail.com>
2025-10-28 20:11:21 +0530

    Control stmts cont

---
+++ Class5/ControlStatements.py
@@ -0,0 +1,49 @@
+# Control Statements
+
+    # Conditional Statements
+        # if
+            # age = 17
+
+            # if(age > 18):
+            #     print("Eligible for voting")
+
+        # if else
+            # age = 17
+
+            # if(age > 18):
+            #     print("Eligible for voting")
+            # else:
+            #     print("Not Eligible")
+
+        # elif
+            # age = 22
+
+            # if(age > 18):
+            #     print("Eligible for voting")
+            # elif(age == 18):
+            #     print("Partially")
+            # else:
+            #     print("Not Eligible")
+
+    # Looping Statements
+        # for loop
+
+            # Example 1:
+
+                # for i in range(1, 11, 3):
+                #     print(i)
+
+            # Example 2:
+
+                # fruits = ["apple", "banana", "orange", "grapes", "watermelon"]
+
+                #     for item in fruits:
+                #         print(item)
+
+
+
+
+        # while loop
+
+
+    # Jumping Statements
