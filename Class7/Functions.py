9bcf171fda69e2c783a0cf52a96a74e435bbe388
ManojNambiraj <postboxmano@gmail.com>
2025-10-30 20:06:36 +0530

    Functions

---
+++ Class7/Functions.py
@@ -0,0 +1,33 @@
+# Functions:
+
+    # User defined functions:
+
+        # DRY --> Dont repeat your code
+
+    # def Multi(a, b):
+    #    return a * b
+
+    # print(Multi(7, 2))
+
+# Types of Arguments or Parametes:
+
+    # Positional argument
+
+        # def demo(a, b, c):
+        #     print(a, b, c)
+
+        # demo(2, 7, 9)
+
+    # Default argument
+
+        # def demo(a, b, c = 1):
+        #     print(a, b, c)
+
+        # demo(2, 7, 9)
+
+    # Keyword argument
+
+        # def demo(a, b, c):
+        #     print(a, b, c)
+
+        # demo(c = 2, a = 7, b = 9)
